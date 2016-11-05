042a710f9d4ac9896615b564422856cf39671eac
Micah Bales <micahbales@gmail.com>
2016-11-05 14:12:24 -0400

    created failing tests for new congregation

--- spec/features/02_user_creates_congregation_spec.rb
+++ spec/features/02_user_creates_congregation_spec.rb
@@ -15,7 +15,7 @@ RSpec.feature "user creates congregation" , %Q(
 
   let(:user) { FactoryGirl.create(:user) }
 
-  scenario "user successfully creates a congregation" do
+  scenario "user successfully creates a congregation, providing all information" do
 
     visit "/"
     click_link("Dashboard")
@@ -28,5 +28,30 @@ RSpec.feature "user creates congregation" , %Q(
     click_button("Submit")
 
     expect(page).to have_content("Your congregation has been created!")
+  end
+
+  scenario "user successfully creates a congregation, filling in name only" do
+
+    visit "/"
+    click_link("Dashboard")
+    click_link("Create a new congregation")
+    fill_in "Name"
+    click_button("Submit")
+
+    expect(page).to have_content("Your congregation has been created!")
+  end
+
+  scenario "user fail to create congregation (no name provided)" do
+
+    visit "/"
+    click_link("Dashboard")
+    click_link("Create a new congregation")
+    fill_in "Address"
+    fill_in "Phone Number"
+    fill_in "Website"
+    fill_in "Affiliation"
+    click_button("Submit")
+
+    expect(page).to have_content("Name can't be blank")
   end
 end
