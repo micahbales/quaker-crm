c211d581d7f956c787b838386f411add93638ece
Micah Bales <micahbales@gmail.com>
2016-11-07 12:09:47 -0500

    added user story and acceptance criteria

---
+++ spec/features/04_user_views_individual_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.feature "" %Q(
+
+  As a Quaker leader
+  I want to view an individual's details
+  So that I can locate their contact info and other information
+
+) do
+
+  # Acceptance Criteria:
+  #
+  # [x] I must be a logged in user
+  # [x] I must have created a meeting
+  # [x] I must have created an individual
+  # [] I can visit an individual's profile page via a listed link on the meeting's page
+  # [] I can view an individual's first name and last name, address, email, phone, birthday, and notes
+
+  scenario "" do
+  end
+
+end
