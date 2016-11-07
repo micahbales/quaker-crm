e60995572fcaaab56795e3d7f261ce20fcd41e7b
Micah Bales <micahbales@gmail.com>
2016-11-06 20:33:47 -0500

    added individuals to routes.rb, added link to add individual on meetings show page

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root "home#index"
   resources :meetings
+  resources :individuals
   get 'dashboard', to: "dashboard#index", as: :dashboard
 end
--- spec/features/03_user_adds_individual_spec.rb
+++ spec/features/03_user_adds_individual_spec.rb
@@ -20,7 +20,8 @@ RSpec.feature "user adds individual" , %Q(
     login_user
     visit dashboard_path
     click_link("New City Friends Meeting")
-    click_link("Add individual to this Meeting")
+    save_and_open_page
+    click_link("Add individual to New City Friends Meeting")
     fill_in("First Name", with: "Jim")
     fill_in("Last Name", with: "Bo")
     fill_in("Address", with: "101 Jimmy's Place")
