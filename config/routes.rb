cc8d8ec0c7aa88ccc96538697ebdf6559b7f81b0
Micah Bales <micahbales@gmail.com>
2016-11-06 21:03:13 -0500

    changed routes structure: nested individuals under meetings

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :meetings
-  resources :individuals
+  resources :meetings do
+    resources :individuals
+  end
   get 'dashboard', to: "dashboard#index", as: :dashboard
 end
