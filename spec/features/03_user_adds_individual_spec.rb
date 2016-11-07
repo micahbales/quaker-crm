0e2115b65f71be827056558dc1f1bfa7bb423f68
Micah Bales <micahbales@gmail.com>
2016-11-07 11:58:57 -0500

    users listed on meeting show page

--- spec/features/03_user_adds_individual_spec.rb
+++ spec/features/03_user_adds_individual_spec.rb
@@ -10,9 +10,9 @@ RSpec.feature "user adds individual" , %Q(
   #
   # [x] I must be a logged in user
   # [x] I must have created a meeting
-  # [] I can add an individual to my meeting with a first name and last name (required), address, email, phone, birthday, and notes
-  # [] When I successfully add a user, I receive a confirmation message
-  # [] When I fail to provide a name, I receive an error message
+  # [x] I can add an individual to my meeting with a first name and last name (required), address, email, phone, birthday, and notes
+  # [x] When I successfully add a user, I receive a confirmation message
+  # [x] When I fail to provide a name, I receive an error message
 
   let!(:user) { FactoryGirl.create(:user) }
   let!(:meeting) { FactoryGirl.create(:meeting, user: user) }
@@ -34,6 +34,7 @@ RSpec.feature "user adds individual" , %Q(
     click_button("Submit")
 
     expect(page).to have_content("Jim Bo has been added to New City Friends Meeting!")
+    expect(page).to have_content("People of New City Friends Meeting")
   end
 
   scenario "individual is created as long as there is a name" do
@@ -46,6 +47,7 @@ RSpec.feature "user adds individual" , %Q(
     click_button("Submit")
 
     expect(page).to have_content("Jim Bo has been added to New City Friends Meeting!")
+    expect(page).to have_content("People of New City Friends Meeting")
   end
 
   scenario "individual is not created when no name is provided" do
@@ -63,6 +65,7 @@ RSpec.feature "user adds individual" , %Q(
     click_button("Submit")
 
     expect(page).to have_content("First name can't be blank and Last name can't be blank")
+    expect(page).to_not have_content("People of New City Friends Meeting")
   end
 
 end
