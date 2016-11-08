a1030daf2c87091fae809f66dc77366face225d3
Micah Bales <micahbales@gmail.com>
2016-11-08 15:14:59 -0500

    added user story and acceptance criteria

--- spec/features/users/03_registered_user_views_meeting_spec.rb
+++ spec/features/users/03_registered_user_views_meeting_spec.rb
@@ -1,1 +1,21 @@
+require 'rails_helper'
 
+RSpec.feature "registered user may view meetings that they created" , %Q(
+  As a Quaker leader
+  I want to see only the Meetings that I have created
+  To keep things simple, and to preserve the privacy of all users
+) do
+
+  # Acceptance Criteria:
+  #
+  # [] I must be a logged in user
+  # [] When I visit the dashboard, I see any Meetings that I have created
+  # [] I do not see Meetings that my user did not create
+  # [] If I visit a URL for a meeting that is not my own, I am redirected
+  #    to my dashboard and receive an error message
+
+
+  scenario "" do
+
+  end
+end
