35db74ce830c37cb88379bbf88233bc0261b84f9
Micah Bales <micahbales@gmail.com>
2016-11-08 13:37:53 -0500

    added delete button to individual edit page

--- spec/features/individuals/04_user_deletes_individual_spec.rb
+++ spec/features/individuals/04_user_deletes_individual_spec.rb
@@ -14,14 +14,18 @@ RSpec.feature "user deletes individual" , %Q(
   # [x] I can visit an individual's profile page via the meeting page
   # [x] Via a link on the profile page, I can visit the update page
   # [] On the update page, I have an option to delete the individual
-  # [] Before the individual is deleted, I am asked to confirm
+  # [] If I click to delete, I receive a notification that the individual has
+  #    been deleted from the meeting
 
   let!(:user) { FactoryGirl.create(:user) }
   let!(:meeting) { FactoryGirl.create(:meeting, user: user) }
   let!(:individual) { FactoryGirl.create(:individual, meeting: meeting) }
 
-  scenario "" do
+  scenario "user clicks to delete individual" do
+    login_user
+    visit edit_meeting_individual_path(meeting, individual)
+    click_button("Delete Profile")
 
+    expect(page).to have_content("#{individual.first_name} #{individual.last_name} has been deleted from #{meeting.name}")
   end
-
 end
