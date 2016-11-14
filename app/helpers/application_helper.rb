9366b7945db68b6abd7d016b973da4beca720ded
Micah Bales <micahbales@gmail.com>
2016-11-13 21:57:27 -0500

    user views meeting details

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
 
   def attribute_filter(key, value)
-    unless key == "id" || key == "created_at" || key == "updated_at" || key == "meeting_id" || value == ""
+    unless key == "id" || key == "created_at" || key == "updated_at" || key == "meeting_id" || key == "user_id" || key == "name" || value == ""
       "#{key.gsub(/_/, " ").capitalize}: #{value}"
     end
   end
