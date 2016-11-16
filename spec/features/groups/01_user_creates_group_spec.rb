ee8050aefc44ddc0d5fd2421ad1557ec02df58ed
Micah Bales <micahbales@gmail.com>
2016-11-16 12:35:47 -0500

    added 2 success and 1 failure test

--- spec/features/groups/01_user_creates_group_spec.rb
+++ spec/features/groups/01_user_creates_group_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'rails_helper'
 
 RSpec.feature "user creates group" , %Q(
   As a Quaker leader
@@ -6,25 +6,57 @@ RSpec.feature "user creates group" , %Q(
   So that I can keep track of the committees and small groups my people are involved in
 ) do
 
-  Acceptance Criteria:
+  # Acceptance Criteria:
 
-  # [] I must be a logged in user
-  # [] I must have created a meeting and at least one individual
+  # [x] I must be a logged in user
+  # [x] I must have created a meeting and at least one individual
   # [] On the dashboard, I am presented with an option to create a group
   # [] I am redirected to a form where I can provide a name (required)
   #    and description for the group
   # [] If I provide valid information, I receive a confirmation message
   # [] If I fail to provide valid information, I receive an error
 
+  let(:user) { FactoryGirl.create(:user) }
+  let(:meeting) { FactoryGirl.create(:meeting, user: user) }
+  let(:individual) { FactoryGirl.create(:individual, meeting: meeting) }
+
   scenario "user successfully creates group (with description)" do
 
+    login_user(user)
+    visit dashboard_path
+    click_link("Create a new group")
+    fill_in("Group Name", with: "Ministry & Oversight")
+    fill_in("Description", with: "M&O works with the pastor to help discern God's will for the meeting, and to provide pastoral care to the congregation.")
+    click_button("Submit")
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Ministry & Oversight")
+    expect(page).to have_content("Group successfully created!")
   end
 
   scenario "user successfully creates group (without description)" do
 
+    login_user(user)
+    visit dashboard_path
+    click_link("Create a new group")
+    fill_in("Group Name", with: "Ministry & Oversight")
+    click_button("Submit")
+
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Ministry & Oversight")
+    expect(page).to have_content("Group successfully created!")
   end
 
   scenario "user fails to provide group name" do
 
+    login_user(user)
+    visit dashboard_path
+    click_link("Create a new group")
+    fill_in("Description", with: "M&O works with the pastor to help discern God's will for the meeting, and to provide pastoral care to the congregation.")
+    click_button("Submit")
+
+    expect(current_path).to_not eq(dashboard_path)
+    expect(page).to have_content("Ministry & Oversight")
+    expect(page).to have_content("Group Name can't be blank")
   end
 end
