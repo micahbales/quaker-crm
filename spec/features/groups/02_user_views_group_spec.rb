620b6edd4c93b382e808d7664e7c6e50168acc2a
Micah Bales <micahbales@gmail.com>
2016-11-16 15:35:49 -0500

    added tests

---
+++ spec/features/groups/02_user_views_group_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+
+RSpec.feature "user views group" , %Q(
+  As a Quaker leader
+  I want to view a group
+  So that I can learn its purpose and who is in it
+) do
+
+  # Acceptance Criteria
+  #
+  # [] I must be a logged in user
+  # [] I must have created a meeting
+  # [] I must have created a group
+  # [] When I visit my meeting's page, I can click a link to view my group
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:meeting) { FactoryGirl.create(:meeting, user: user) }
+  let(:group) { FactoryGirl.create(:group) }
+
+  scenario "user views group" do
+
+    login_user(user)
+    visit meeting_path(meeting)
+    click_link(group.name)
+
+    expect(page).to have_content(group.name)
+    expect(page).to have_content(group.description)
+  end
+end
