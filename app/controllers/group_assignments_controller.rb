9e322c254039aec1fd7b557a39948c6c9b4c5ca1
Micah Bales <micahbales@gmail.com>
2016-11-18 12:09:03 -0500

    added 'create' controller for group_assignments

--- app/controllers/group_assignments_controller.rb
+++ app/controllers/group_assignments_controller.rb
@@ -1,4 +1,17 @@
 class GroupAssignmentsController < ApplicationController
 
+  def create
+    @meeting = Meeting.find(params["meeting_id"])
+    @individual = Individual.find(params["individual_id"])
+    @group = Group.find(params["group_assignment"]["groups"])
+    @group_assignment = GroupAssignment.new(group_id: @group.id, individual_id: @individual.id)
 
+    if @group_assignment.save
+      flash[:alert] = "#{@individual.first_name} #{@individual.last_name} has been added to #{@group.name}!"
+      redirect_to meeting_individual_path(@meeting, @individual)
+    else
+      flash[:alert] = @group_assignment.errors.full_messages.to_sentence
+      render meeting_individual_path(@meeting, @individual)
+    end
+  end
 end
