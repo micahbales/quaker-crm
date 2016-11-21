d11ae7597c6756b2afd87f40a2f292c10d61da3e
Micah Bales <micahbales@gmail.com>
2016-11-20 21:42:23 -0500

    added test for adding individual to 3 groups

--- spec/features/groups/03_user_adds_individual_to_group_spec.rb
+++ spec/features/groups/03_user_adds_individual_to_group_spec.rb
@@ -14,10 +14,10 @@ RSpec.feature "user adds individual to group" , %Q(
   # [] When I visit the profile page of an individual, I have an option to add
   #    that individual to a group
   # [x] I can select an existing group from a drop-down select box
-  # [] When I click to submit:
+  # [x] When I click to submit:
   #     [x] The individual is added to the group
   #     [x] I see a success message
-  #     [] I see the individual show page, with a list of which groups that
+  #     [x] I see the individual show page, with a list of which groups that
   #        individual is part of
   #     [x] If I navigate to the group show page, I can see which individuals are part
   #        of that group
@@ -25,6 +25,8 @@ RSpec.feature "user adds individual to group" , %Q(
   let(:user) { FactoryGirl.create(:user) }
   let(:meeting) { FactoryGirl.create(:meeting, user: user) }
   let!(:group) { FactoryGirl.create(:group, meeting: meeting) }
+  let!(:group2) { FactoryGirl.create(:group, meeting: meeting) }
+  let!(:group3) { FactoryGirl.create(:group, meeting: meeting) }
   let!(:individual) { FactoryGirl.create(:individual, meeting: meeting) }
 
   scenario "user successfully adds an individual to a group" do
@@ -43,6 +45,23 @@ RSpec.feature "user adds individual to group" , %Q(
     visit meeting_group_path(meeting, group)
 
     expect(page).to have_content("#{individual.first_name} #{individual.last_name}")
+  end
+
+  scenario "user adds individual to 3 groups" do
+
+    login_user(user)
+    visit meeting_path(meeting)
+    click_link("#{individual.first_name} #{individual.last_name}")
+    select(group.name)
+    click_button("+")
+
+    select(group2.name)
+    click_button("+")
+
+    select(group3.name)
+    click_button("+")
+
+    expect(page).to have_content("Groups: #{individual.groups.first.name}, #{individual.groups[1].name}, #{individual.groups.last.name}")
   end
 
   scenario "user fails to select a valid group" do
