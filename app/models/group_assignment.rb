30f9d7c0890ca01d4438b845d018388f218fe06f
Micah Bales <micahbales@gmail.com>
2016-11-20 21:33:46 -0500

    validate for uniqueness of group_id/individual_id combo

--- app/models/group_assignment.rb
+++ app/models/group_assignment.rb
@@ -1,6 +1,6 @@
 class GroupAssignment < ApplicationRecord
-  validates :group_id, presence: true, uniqueness: true
-  validates :individual_id, presence: true, uniqueness: true
+  validates :group_id, presence: true, uniqueness: {scope: :individual_id}
+  validates :individual_id, presence: true, uniqueness: {scope: :group_id}
 
   belongs_to :group
   belongs_to :individual
