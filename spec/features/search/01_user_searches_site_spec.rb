d9c8983e8f35c0bdd4a617929635e9bbf3bcd188
Micah Bales <micahbales@gmail.com>
2016-11-22 18:42:52 -0500

    added user story and acceptance criteria

---
+++ spec/features/search/01_user_searches_site_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.feature "user searches site" , %Q(
+  As a Quaker leader
+  I want to search the website for key words
+  So that I can quickly locate an individual, group, or meeting that belongs to me
+) do
+
+  # Acceptance Criteria:
+  #
+  # [] I must be a logged in user
+  # [] I must have created a meeting, an individual, and a group
+  # [] When I enter a search term into the search bar at the top right hand side
+  #    of the page, and click "Submit", I am presented with a list of search results
+  #    that includes any individual, group, or meeting that meets those criteria
+  # [] The search only returns results that my user account has created
+
+  scenario "" do
+
+  end
+end
