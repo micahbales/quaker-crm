1a00eb288c43d463e0f472edf053ea8eadf95f65
Micah Bales <micahbales@gmail.com>
2016-11-21 19:56:52 -0500

    cycle factory birthdays between 1..29 to avoid errors

--- spec/factories/individuals.rb
+++ spec/factories/individuals.rb
@@ -5,7 +5,7 @@ FactoryGirl.define do
     sequence(:email) {|n| "bobby#{n}@aol.com"}
     sequence(:address) {|n| "10#{n} Fictional Ave"}
     phone "1234567890"
-    sequence(:birthday) {|n| DateTime.new(1995,4,n) }
+    sequence(:birthday, (1..29).cycle) {|n| DateTime.new(1995,4,n) }
     notes "Aenean ut dui fermentum, sagittis tellus vitae, pulvinar dui. Donec et metus nisl. Nam gravida, ligula sit amet porta suscipit, elit purus malesuada quam, semper tristique elit lorem et erat."
     meeting
   end
