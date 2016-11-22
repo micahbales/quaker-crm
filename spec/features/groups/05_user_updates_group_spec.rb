5ae3073676165a7372e6d6f7eb84f2e7375bffa7
Micah Bales <micahbales@gmail.com>
2016-11-21 20:25:21 -0500

    added user story and acceptance criteria

---
+++ spec/features/groups/05_user_updates_group_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.feature "user updates group" , %Q(
+  As a Quaker leader
+  I want to update a group
+  So that its description and title are accurate
+) do
+
+  # Acceptance Criteria:
+  #
+  # [x] I must be a logged in user
+  # [x] I must have created a meeting
+  # [x] I must have created a group
+  # [] I have an option to update my group
+  # [] If I provide valid information, the group is updated and I receive
+  #    a confirmation message
+  # [] If I leave the group's name blank, I am given an error message and
+  #    the group is not updated  
+
+  scenario "" do
+
+  end
+end
