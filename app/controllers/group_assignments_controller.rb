46622a6b599df1c13fd9a97cd182fc88064c32ea
Micah Bales <micahbales@gmail.com>
2016-11-21 19:59:34 -0500

    added destroy endpoint

--- app/controllers/group_assignments_controller.rb
+++ app/controllers/group_assignments_controller.rb
@@ -22,6 +22,29 @@ class GroupAssignmentsController < ApplicationController
     end
   end
 
+  def destroy
+    @meeting = Meeting.find(params["meeting_id"])
+    @individual = Individual.find(params["individual_id"])
+
+    if params["group_assignment"]["group"].empty?
+      flash[:alert] = "Please select a valid group!"
+      redirect_to meeting_individual_path(@meeting, @individual)
+    else
+
+      @group = Group.find(params["group_assignment"]["group"])
+      @group_assignment = GroupAssignment.where(group_id: @group.id, individual_id: @individual.id)[0]
+
+      if @group_assignment.nil? 
+        flash[:alert] = "#{@individual.first_name} #{@individual.last_name} was not a member of #{@group.name}!"
+        redirect_to meeting_individual_path(@meeting, @individual)
+      else
+        @group_assignment.destroy
+        flash[:alert] = "#{@individual.first_name} #{@individual.last_name} has been removed from #{@group.name}!"
+        redirect_to meeting_individual_path(@meeting, @individual)
+      end
+    end
+  end
+
   def add_or_remove
     if params["commit"] == "+"
       create
