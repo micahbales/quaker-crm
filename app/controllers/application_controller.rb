b7d26294ad0f21edcccc62f1b05803f7c8deee71
Micah Bales <micahbales@gmail.com>
2016-11-22 18:46:53 -0500

    secure group & group_assignments controllers with before_action helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,13 +16,11 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  def authorize_individual_owner!
-    unless @authorize == true
-      @meeting = Meeting.find(params[:meeting_id])
-      if !current_user.meetings.include?(@meeting)
-        flash[:notice] = "You are not authorized to view this resource"
-        redirect_to dashboard_path
-      end
+  def authorize_individual_group_owner!
+    @meeting = Meeting.find(params[:meeting_id])
+    if !current_user.meetings.include?(@meeting)
+      flash[:notice] = "You are not authorized to view this resource"
+      redirect_to dashboard_path
     end
   end
 end
