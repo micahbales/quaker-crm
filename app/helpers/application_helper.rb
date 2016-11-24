288c0ecc7f0a797a65c7957c08396f88fc63164e
Micah Bales <micahbales@gmail.com>
2016-11-24 11:03:50 -0500

    create multi_search_result_filter

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,22 @@ module ApplicationHelper
       "#{key.gsub(/_/, " ").capitalize}: #{value}"
     end
   end
+
+  def multisearch_result_filter(result)
+    if result.searchable_type == "Meeting"
+      #return meeting data
+      meeting = Meeting.find(result.searchable_id)
+      "#{meeting.name}"
+    elsif result.searchable_type == "Individual"
+      #return individual data
+      individual = Individual.find(result.searchable_id)
+      meeting = Meeting.find(individual.meeting_id)
+      "#{individual.first_name} #{individual.last_name}"
+    elsif result.searchable_type == "Group"
+      #return group data
+      group = Group.find(result.searchable_id)
+      meeting = Meeting.find(group.meeting_id)
+      "#{group.name}"
+    end
+  end
 end
