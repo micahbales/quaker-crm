76c80015aa3917d6eb48adc2f55d072001bfbc7f
Micah Bales <micahbales@gmail.com>
2016-12-08 17:54:24 -0500

    test Meeting dependent: :destroy of groups/individuals

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -3,4 +3,28 @@ require "rails_helper"
 RSpec.describe Meeting do
   it { should have_valid(:name).when("Bob's House", "The Library") }
   it { should_not have_valid(:name).when(nil, "") }
+
+  describe "dependent destroy" do
+    it "destroys associated individuals" do
+      meeting = FactoryGirl.create(:meeting)
+      individual = FactoryGirl.create(:individual)
+
+      meeting.individuals << individual
+      individual_id = individual.id
+      meeting.destroy
+
+      expect{ Individual.find(individual_id )}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+
+    it "destroys associated groups" do
+      meeting = FactoryGirl.create(:meeting)
+      group = FactoryGirl.create(:group)
+
+      meeting.groups << group
+      group_id = group.id
+      meeting.destroy
+
+      expect{ Group.find(group_id )}.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
