d96dfde5a7759a36b560a857302a035c5c5ba7a7
Micah Bales <micahbales@gmail.com>
2016-12-09 14:50:31 -0500

    add type: :model to tests

--- spec/models/individual_spec.rb
+++ spec/models/individual_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Individual do
+RSpec.describe Individual, type: :model do
   it { should have_valid(:first_name).when("a", "Bobby", "TK421") }
   it { should_not have_valid(:first_name).when(nil, "") }
   it { should have_valid(:last_name).when("a", "Bobby", "TK421") }
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-RSpec.describe Meeting do
+RSpec.describe Meeting, type: :model do
   it { should have_valid(:name).when("Bob's House", "The Library") }
   it { should_not have_valid(:name).when(nil, "") }
   it { should belong_to(:user)}
