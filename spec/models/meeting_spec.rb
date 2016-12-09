d374f3ce358556f8970dfb3366b34c8d7d28de39
Micah Bales <micahbales@gmail.com>
2016-12-08 20:31:50 -0500

    test Meeting belongs to User

--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe Meeting do
   it { should have_valid(:name).when("Bob's House", "The Library") }
   it { should_not have_valid(:name).when(nil, "") }
+  it { should belong_to(:user)}
 
   describe "dependent destroy" do
     it "destroys associated individuals" do
