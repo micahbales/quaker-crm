11ff53c11a80817b7a304b1508fabe449ba6562a
Micah Bales <micahbales@gmail.com>
2016-12-08 21:06:21 -0500

    test that Group belongs to Meeting & has individuals & group_assignments

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -3,4 +3,7 @@ require "rails_helper"
 RSpec.describe Group do
   it { should have_valid(:name).when("a", "124151", "yo2$%z") }
   it { should_not have_valid(:name).when(nil, "") }
+  it { should belong_to(:meeting) }
+  it { should have_many(:group_assignments) }
+  it { should have_many(:individuals) }
 end
