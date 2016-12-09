5bc294e5a8e07513d8f768646ae915ebc7b3254e
Micah Bales <micahbales@gmail.com>
2016-12-08 20:52:39 -0500

    test Individual has groups & group_assignments

--- spec/models/individual_spec.rb
+++ spec/models/individual_spec.rb
@@ -6,4 +6,6 @@ RSpec.describe Individual do
   it { should have_valid(:last_name).when("a", "Bobby", "TK421") }
   it { should_not have_valid(:last_name).when(nil, "") }
   it { should belong_to(:meeting) }
+  it { should have_many(:groups) }
+  it { should have_many(:group_assignments) }
 end
