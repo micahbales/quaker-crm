01420954438b70a655f1460e8020654e6d698024
Micah Bales <micahbales@gmail.com>
2016-12-08 20:53:50 -0500

    test Individual dependent: :destroy of group_assignments

--- spec/models/individual_spec.rb
+++ spec/models/individual_spec.rb
@@ -8,4 +8,16 @@ RSpec.describe Individual do
   it { should belong_to(:meeting) }
   it { should have_many(:groups) }
   it { should have_many(:group_assignments) }
+
+  describe "dependent destroy" do
+    it "destroys associated group_assignments" do
+      individual = FactoryGirl.create(:individual)
+      group_assignment = FactoryGirl.create(:group_assignment)
+
+      individual.group_assignments << group_assignment
+      group_assignment_id = group_assignment.id
+      individual.destroy
+      expect{ GroupAssignment.find(group_assignment_id) }.to raise_error(ActiveRecord::RecordNotFound)
+    end
+  end
 end
