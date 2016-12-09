fb81c93a6028e949d67f41daf56d585b1c8d098c
Micah Bales <micahbales@gmail.com>
2016-12-08 20:37:04 -0500

    test Individual belongs to Meeting

--- spec/models/individual_spec.rb
+++ spec/models/individual_spec.rb
@@ -5,4 +5,5 @@ RSpec.describe Individual do
   it { should_not have_valid(:first_name).when(nil, "") }
   it { should have_valid(:last_name).when("a", "Bobby", "TK421") }
   it { should_not have_valid(:last_name).when(nil, "") }
+  it { should belong_to(:meeting) }
 end
