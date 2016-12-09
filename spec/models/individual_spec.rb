0db8fc5c7b347dc804fc33b7897c977558a500e5
Micah Bales <micahbales@gmail.com>
2016-12-08 20:36:04 -0500

    test Individual first_name/last_name validations

---
+++ spec/models/individual_spec.rb
@@ -0,0 +1,8 @@
+require "rails_helper"
+
+RSpec.describe Individual do
+  it { should have_valid(:first_name).when("a", "Bobby", "TK421") }
+  it { should_not have_valid(:first_name).when(nil, "") }
+  it { should have_valid(:last_name).when("a", "Bobby", "TK421") }
+  it { should_not have_valid(:last_name).when(nil, "") }
+end
